b85e15f538f018a97b10a3b866cf93e343864ecb
r-gulles <gullesronian21>
2026-04-10 00:14:52 +0800

    modify api

--- utils/api.js
+++ utils/api.js
@@ -7,8 +7,7 @@ import axios from 'axios';
 import { Platform } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const LOCAL_HOST = Platform.OS === 'android' ? '10.0.2.2' : '127.0.0.1';
-const API_URL = `http://${LOCAL_HOST}:8000/api/`;
+const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
