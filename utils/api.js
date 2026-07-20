bebb150a166da2b89a426ae02385f2fb9cd09df3
GlennPena <gppena.student@ua.edu.ph>
2026-07-20 22:14:35 +0800

    Fix production backend URL to uaclinicappointmentsystem-backend.onrender.com

--- utils/api.js
+++ utils/api.js
@@ -8,7 +8,10 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const GOOGLE_WEB_CLIENT_ID = process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID || "421692598259-bifnnf8faeoau6ck5idf515slfakg0pv.apps.googleusercontent.com";
 
-let rawApiUrl = process.env.EXPO_PUBLIC_API_URL || "https://uaclinicappointmentsystem-backend-6obi.onrender.com/api/";
+let rawApiUrl = process.env.EXPO_PUBLIC_API_URL || "https://uaclinicappointmentsystem-backend.onrender.com/api/";
+// Remove accidental -6obi if present in env variable
+rawApiUrl = rawApiUrl.replace("-6obi.onrender.com", ".onrender.com");
+
 if (!rawApiUrl.endsWith('/api/') && !rawApiUrl.endsWith('/api')) {
   rawApiUrl = rawApiUrl.replace(/\/$/, '') + '/api/';
 } else if (!rawApiUrl.endsWith('/')) {
