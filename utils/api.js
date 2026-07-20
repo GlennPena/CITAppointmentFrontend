6cff78c279815b3d44b4eff63ecc4c7877911086
GlennPena <gppena.student@ua.edu.ph>
2026-07-20 21:34:24 +0800

    Add fallback Google Client ID for Vercel deployment

--- utils/api.js
+++ utils/api.js
@@ -6,9 +6,9 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const GOOGLE_WEB_CLIENT_ID = process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID;
+export const GOOGLE_WEB_CLIENT_ID = process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID || "421692598259-bifnnf8faeoau6ck5idf515slfakg0pv.apps.googleusercontent.com";
 
-const API_URL = process.env.EXPO_PUBLIC_API_URL;
+const API_URL = process.env.EXPO_PUBLIC_API_URL || "http://localhost:8000/api/";
 
 const api = axios.create({
   baseURL: API_URL,
